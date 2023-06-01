05ab73f69ca511ec46b12a49cac3854465f850c5
selvababu09 <selvababu09@gmail.com>
2023-06-01 13:43:42 +0530

    Kannan added

--- src/org/comp/GitDemo.java
+++ src/org/comp/GitDemo.java
@@ -1,6 +1,10 @@
 package org.comp;
 
 public class GitDemo {
+	
+	private void Kannan() {
+  System.out.println("Kannan added");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Data");
